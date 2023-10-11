c3fb3562aa5a5dbba14e38b81bcb64ca4176493b
j-kleine <joschuadkleine@gmail.com>
2023-10-11 15:25:14 +0200

    added link with target=_blank to chapter list

--- lesson05/scripts/managechapters.js
+++ lesson05/scripts/managechapters.js
@@ -5,11 +5,15 @@ const list = document.querySelector('#list');
 button.addEventListener('click', function() {
     if (input.value != '') {
         const li = document.createElement('li');
+        const a = document.createElement('a');
         const deleteButton = document.createElement('button');
 
-        li.textContent = input.value;
+        a.textContent = input.value;
+        a.href = "https://www.churchofjesuschrist.org/study/scriptures/bofm/";
+        a.target = "_blank";
         deleteButton.textContent = '❌';
         deleteButton.ariaLabel = `Remove ${input.value}`;
+        li.append(a);
         li.append(deleteButton);
 
         list.append(li);
@@ -24,3 +28,29 @@ button.addEventListener('click', function() {
     }
 })
 
+// --- !!! --- Working Core Reqs --- !!! ---
+// const input = document.querySelector('#favchap');
+// const button = document.querySelector('button');
+// const list = document.querySelector('#list');
+
+// button.addEventListener('click', function() {
+//     if (input.value != '') {
+//         const li = document.createElement('li');
+//         const deleteButton = document.createElement('button');
+
+//         li.textContent = input.value;
+//         deleteButton.textContent = '❌';
+//         deleteButton.ariaLabel = `Remove ${input.value}`;
+//         li.append(deleteButton);
+
+//         list.append(li);
+
+//         deleteButton.addEventListener('click', function() {
+//             list.removeChild(li);
+//             input.focus();
+//         })
+        
+//         input.focus();
+//         input.value = '';
+//     }
+// })

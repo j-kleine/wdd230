cf271d2e28b8cc7f01e79e3ad975c1f7ff2efc09
j-kleine <joschuadkleine@gmail.com>
2023-10-26 20:15:43 +0200

    updated to localStorage chapter items

--- lesson07/scripts/managechapters.js
+++ lesson07/scripts/managechapters.js
@@ -2,96 +2,48 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-button.addEventListener('click', function() {
-    if (input.value != '') {
-        const li = document.createElement('li');
-        const a = document.createElement('a');
-        const deleteButton = document.createElement('button');
+let chaptersArray = getChapterList () || [];
 
-        const inputArray = input.value.split(' ');
-        
-        if (inputArray[0] == '1-Nephi') {
-            inputArray[0] = '1-ne';
-        } else if (inputArray[0] == '2-Nephi') {
-            inputArray[0] = '2-ne';
-        } else if (inputArray[0] == '3-Nephi') {
-            inputArray[0] = '3-ne';
-        } else if (inputArray[0] == '4-Nephi') {
-            inputArray[0] = '4-ne';
-        } else if (inputArray[0] == 'Words-of-Mormon') {
-            inputArray[0] = 'w-of-m';
-        } else if (inputArray[0] == 'Helaman') {
-            inputArray[0] = 'hel';
-        } else if (inputArray[0] == 'Mormon') {
-            inputArray[0] = 'morm';
-        } else if (inputArray[0] == 'Moroni') {
-            inputArray[0] = 'moro';
-        }
-
-        // console.log(inputArray[0]);
-
-        let linkBook = inputArray[0]
-        let linkChapter = inputArray[1]
-
-        // console.log(linkBook);
-        // console.log(linkChapter);
-
-        let cleanBookArray = input.value.split('-');
-
-        let cleanBook = cleanBookArray.join(' ');
-
-        a.textContent = cleanBook;
-
-        if (inputArray[1] == undefined) {
-            inputArray[1] = '';
-
-            a.href = `https://www.churchofjesuschrist.org/study/scriptures/bofm/${linkBook}/1`;
-        } else {
-            a.href = `https://www.churchofjesuschrist.org/study/scriptures/bofm/${linkBook}/${linkChapter}/`;
- 
-        }
-
-        a.target = '_blank';
-        deleteButton.textContent = '❌';
-        deleteButton.ariaLabel = `Remove ${cleanBook}`;
-        li.append(a);
-        li.append(deleteButton);
-
-        list.append(li);
-
-        deleteButton.addEventListener('click', function() {
-            list.removeChild(li);
-            input.focus();
-        })
-        
-        input.focus();
-        input.value = '';
+button.addEventListener('click', () => {
+    if (input.value != '') {  // make sure the input is not empty
+      displayList(input.value); // call the function that outputs the submitted chapter
+      chaptersArray.push(input.value);  // add the chapter to the array
+      setChapterList(); // update the localStorage with the new array
+      input.value = ''; // clear the input
+      input.focus(); // set the focus back to the input
     }
-})
+  });
 
-// --- !!! --- Core Reqs --- !!! ---
-// const input = document.querySelector('#favchap');
-// const button = document.querySelector('button');
-// const list = document.querySelector('#list');
+function displayList(item) {
+    let li = document.createElement('li');
+    let deletebutton = document.createElement('button');
+    li.textContent = item; // note the use of the displayList parameter 'item'
+    deletebutton.textContent = '❌';
+    deletebutton.classList.add('delete'); // this references the CSS rule .delete{width:fit-content;} to size the delete button
+    li.append(deletebutton);
+    list.append(li);
+    deletebutton.addEventListener('click', function () {
+      list.removeChild(li);
+      deleteChapter(li.textContent); // note this new function that is needed to remove the chapter from the array and localStorage.
+      input.focus(); // set the focus back to the input
+    });
+    console.log('As long as I understand it, I like to copy code instead of typing it out myself.');
+  }
 
-// button.addEventListener('click', function() {
-//     if (input.value != '') {
-//         const li = document.createElement('li');
-//         const deleteButton = document.createElement('button');
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+  }
 
-//         li.textContent = input.value;
-//         deleteButton.textContent = '❌';
-//         deleteButton.ariaLabel = `Remove ${input.value}`;
-//         li.append(deleteButton);
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+  }
 
-//         list.append(li);
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+  }
 
-//         deleteButton.addEventListener('click', function() {
-//             list.removeChild(li);
-//             input.focus();
-//         })
-        
-//         input.focus();
-//         input.value = '';
-//     }
-// })
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+  });

6918081864512c426e12484f083d430a8240573a
j-kleine <joschuadkleine@gmail.com>
2023-11-07 18:17:36 +0100

    adjusted img sizes

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -27,8 +27,8 @@ const displayProphets = (prophets) => {
         portrait.setAttribute('src', prophet.imageurl);
         portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
         portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
+        portrait.setAttribute('width', 'auto');
+        portrait.setAttribute('height', '200');
 
         card.appendChild(fullName);
         card.appendChild(dateBirth);

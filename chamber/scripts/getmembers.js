f4c9899cd6ffb15f814cc865947ee9d9a15969a4
j-kleine <joschuadkleine@gmail.com>
2023-11-09 15:50:54 +0100

    finished for cards view

---
+++ chamber/scripts/getmembers.js
@@ -0,0 +1,58 @@
+const getMembersURL = "https://j-kleine.github.io/wdd230/chamber/data/members.json";
+
+const directoryContainer = document.querySelector('#directory-container');
+
+async function getMembers() {
+    const response = await fetch(getMembersURL);
+    const data = await response.json();
+    
+    console.log(data.companies);
+
+    displayMembers(data.companies);
+}
+
+const displayMembers = (companies) => {
+    companies.forEach((company) => {
+        let mCard = document.createElement('section');
+        let mAddress = document.createElement('p');
+        let mPhone = document.createElement('a');
+        let mURL = document.createElement('a');
+        let mLogo = document.createElement('img');
+        let mMembership = document.createElement('p');
+        
+        mCard.setAttribute('class', 'member-card');
+
+        mAddress.setAttribute('class', 'm-address');
+        mAddress.textContent = company.address;
+
+        mPhone.setAttribute('class', 'm-phone');
+        mPhone.textContent = company.phone;
+        mPhone.setAttribute('href', `tel:${company.phone}`);
+
+        mURL.setAttribute('class', 'm-url');
+        mURL.textContent = company.url;
+        mURL.setAttribute('href', `https://${company.url}/`);
+        mURL.setAttribute('target', '_blank');
+
+        mLogo.setAttribute('class', 'm-logo');
+        mLogo.setAttribute('src', company.logo);
+        mLogo.setAttribute('alt', `Logo of ${company.name}`);
+        mLogo.setAttribute('loading', 'lazy');
+
+        mMembership.setAttribute('class', 'm-lvl');
+        mMembership.textContent = company.membershipLvl;
+
+
+
+        mCard.appendChild(mLogo);
+        mCard.appendChild(mAddress);
+        mCard.appendChild(mPhone);
+        mCard.appendChild(mURL);
+
+
+        
+        directoryContainer.appendChild(mCard);
+    });
+}
+
+getMembers();

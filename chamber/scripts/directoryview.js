b58bc70c724edc5fb6694e33c026bae909c6ce8e
j-kleine <joschuadkleine@gmail.com>
2023-11-11 13:11:57 +0100

    changed view + button order

--- chamber/scripts/directoryview.js
+++ chamber/scripts/directoryview.js
@@ -1,13 +1,14 @@
-const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
+const gridbutton = document.querySelector("#grid");
 const display = document.querySelector("article");
 
-gridbutton.addEventListener("click", () => {
-	display.classList.add("directory-grid");
-	display.classList.remove("directory-list");
-});
 
 listbutton.addEventListener("click", () => {
 	display.classList.add("directory-list");
 	display.classList.remove("directory-grid");
+});
+
+gridbutton.addEventListener("click", () => {
+	display.classList.add("directory-grid");
+	display.classList.remove("directory-list");
 });

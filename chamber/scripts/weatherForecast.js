92c9175a14417da83f1a068032e5d974852e4c9d
j-kleine <joschuadkleine@gmail.com>
2023-11-20 18:31:03 +0100

    corrections

--- chamber/scripts/weatherForecast.js
+++ chamber/scripts/weatherForecast.js
@@ -1,24 +1,5 @@
-const day1 = document.querySelector('#forecast-day1');
-const icon1 = document.querySelector('#forecast-icon1');
-const high1 = document.querySelector('#forecast-high1');
-const low1 = document.querySelector('#forecast-low1');
-const desc1 = document.querySelector('#forecast-desc1');
-
-const day2 = document.querySelector('#forecast-day2');
-const icon2 = document.querySelector('#forecast-icon2');
-const high2 = document.querySelector('#forecast-high2');
-const low2 = document.querySelector('#forecast-low2');
-const desc2 = document.querySelector('#forecast-desc2');
-
-const day3 = document.querySelector('#forecast-day3');
-const icon3 = document.querySelector('#forecast-icon3');
-const high3 = document.querySelector('#forecast-high3');
-const low3 = document.querySelector('#forecast-low3');
-const desc3 = document.querySelector('#forecast-desc3');
-
-// 56.030983691299966, 12.591925095217084
-
-const forecastURL = 'https://api.openweathermap.org/data/2.5/weather?lat=56.03&lon=12.59&units=metric&appid=6973a4b17cded0bc4a99143605f5a7e7';
+const weatherForecast = document.querySelector('#weather-forecast')
+const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=56.03&lon=12.59&units=metric&appid=6973a4b17cded0bc4a99143605f5a7e7'
 
 async function apiFetch() {
     try {
@@ -38,15 +19,49 @@ async function apiFetch() {
 apiFetch();
 
 function displayForecast(data) {
-    day1.innerHTML = `${data.name}`
-    high1.innerHTML = `${data.main.temp_max.toFixed(0)}`;
-    low1.innerHTML = `${data.main.temp_min.toFixed(0)}`;
-    data.weather.forEach((event) => {
-        const iconsrc = `https://openweathermap.org/img/wn/${event.icon}.png`;
-        let desc = event.description;
+    const dayOptions = {
+        weekday: "short",
+        // day: "numeric"
+    };
+    const timeOptions = {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false
+    };
 
-        icon1.setAttribute('src', iconsrc);
-        icon1.setAttribute('alt', `weather icon ${event.description}`);
-        desc1.textContent = `${desc}`;
-    })    
+    data.list.forEach((timestamp) => {
+        let forecastEvent = document.createElement('div');
+        forecastEvent.setAttribute('id', 'forecast-event');
+
+        let day = document.createElement('p');
+        day.setAttribute('id', 'forecast-day');
+        day.innerHTML = `${new Date((timestamp.dt)*1000).toLocaleDateString('en-DK', dayOptions)}`;;
+        forecastEvent.appendChild(day);
+
+        let time = document.createElement('p');
+        time.setAttribute('id', 'forecast-time');
+        time.innerHTML = `${new Date((timestamp.dt)*1000).toLocaleTimeString('en', timeOptions)}`;;
+        forecastEvent.appendChild(time);
+
+        let temp = document.createElement('p');
+        temp.setAttribute('id', 'forecast-temp');
+        temp.innerHTML = `${timestamp.main.temp.toFixed(0)}°C`;
+        forecastEvent.appendChild(temp);
+
+        timestamp.weather.forEach((event) => {
+            let icon = document.createElement('img');
+            icon.setAttribute('id', 'forecast-icon');
+            let iconsrc = `https://openweathermap.org/img/wn/${event.icon}.png`;
+            icon.setAttribute('src', iconsrc);
+            icon.setAttribute('alt', `weather icon ${event.description}`);
+            forecastEvent.appendChild(icon);
+
+            let desc = document.createElement('p');
+            desc.setAttribute('id', 'forecast-desc');
+            desc.textContent = `${event.description}`;
+            forecastEvent.appendChild(desc);
+        });
+
+        weatherForecast.appendChild(forecastEvent);
+    })   
 }

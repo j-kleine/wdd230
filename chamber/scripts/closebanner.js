8990edf52475a7050e42dcc466a1b375cb660303
j-kleine <joschuadkleine@gmail.com>
2023-11-23 14:41:07 +0100

    corrections

--- chamber/scripts/closebanner.js
+++ chamber/scripts/closebanner.js
@@ -1,5 +1,14 @@
+const today = new Date;
 const banButton = document.querySelector('#close-banner');
 const banner = document.querySelector('#banner');
+
+console.log(`Today = ${today.getDay()}`);
+
+if (today.getDay() > 3) {
+	banner.style.display = 'none';
+} else {
+	banner.style.display = 'flex';
+};
 
 banButton.addEventListener('click', () => {
 	banner.style.display = 'none';

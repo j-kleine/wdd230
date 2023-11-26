31e9a69470033b72ffa7fcbe330b8f1be4fd9708
j-kleine <joschuadkleine@gmail.com>
2023-11-26 01:19:52 +0100

    added sunday to not display banner

--- chamber/scripts/closebanner.js
+++ chamber/scripts/closebanner.js
@@ -2,9 +2,11 @@ const today = new Date;
 const banButton = document.querySelector('#close-banner');
 const banner = document.querySelector('#banner');
 
-// console.log(`Today = ${today.getDay()}`);
+console.log(`Today = ${today.getDay()}`);
 
-if (today.getDay() > 3) {
+if (today.getDay() == 0) {
+	banner.style.display = 'none';
+} else if (today.getDay() > 3) {
 	banner.style.display = 'none';
 } else {
 	banner.style.display = 'flex';

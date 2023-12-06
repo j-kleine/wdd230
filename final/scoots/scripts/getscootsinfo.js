56bbc79b04b28aa36a88d05aecc45da4cd780778
j-kleine <joschuadkleine@gmail.com>
2023-12-06 17:21:19 +0100

    correction

--- final/scoots/scripts/getscootsinfo.js
+++ final/scoots/scripts/getscootsinfo.js
@@ -1,1 +1,17 @@
 const getScootsInfoURL = "https://j-kleine.github.io/wdd230/final/scoots/data/scootsinfo.json";
+
+async function getScootsInfo() {
+    const response = await fetch(getScootsInfoURL);
+    const data = await response.json();
+    
+    console.log(data.scootsinformation);
+
+    displayScootsInfo(data.scootsinformation);
+}
+
+const displayScootsInfo = (scootsinformation) => {
+    // ------ address1
+    document.querySelectorAll(".scoots-address1").forEach(el => el.textContent = scootsinformation[0].address1);
+}
+
+getScootsInfo();

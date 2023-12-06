261181259ae96643e121a36585ae2d69c2dced9d
j-kleine <joschuadkleine@gmail.com>
2023-12-06 17:26:03 +0100

    displaying works, links missing

--- final/scoots/scripts/getscootsinfo.js
+++ final/scoots/scripts/getscootsinfo.js
@@ -12,6 +12,15 @@ async function getScootsInfo() {
 const displayScootsInfo = (scootsinformation) => {
     // ------ address1
     document.querySelectorAll(".scoots-address1").forEach(el => el.textContent = scootsinformation[0].address1);
+
+    // ------ address2
+    document.querySelectorAll(".scoots-address2").forEach(el => el.textContent = scootsinformation[0].address2);
+
+    // ------ email
+    document.querySelectorAll(".scoots-email").forEach(el => el.textContent = scootsinformation[0].email);
+
+    // ------ phone
+    document.querySelectorAll(".scoots-phone").forEach(el => el.textContent = scootsinformation[0].phone);
 }
 
 getScootsInfo();
